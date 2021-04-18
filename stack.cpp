6836ace7e0a8803719c7e86d871e091a69f7fe74
Sinus2x <66667367+Sinus2x@users.noreply.github.com>
2021-04-18 13:08:48 +0300

    main-функция

---
+++ stack.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include "MyStack.h"
+int main() {
+	setlocale(LC_ALL, "Russian");
+	MyStack<int> stack, reversed;
+	multipliers(54, stack);
+	reverse(stack, reversed);
+	return 0;
+}
